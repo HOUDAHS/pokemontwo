6a265e205ca9d4729de45513e083d7487c33d86f
HOUDAHS <houdaslimani16@gmail.com>
2024-10-18 16:13:16 +0100

    d

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import PokeDex from "./Components/PokeDex";
+import PokeInfo from './Components/PokeInfo';
+
+
+import axios from "axios";
+
 
 function App() {
+  
+  const [pokemonList, setPokemonList] = useState([]);
+  const [pokemonInfo, setPokemonInfo] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/?limit=30&offset=30");
+
+  const [nextUrl, setNextUrl] = useState();
+  const [previousUrl, setPreviousUrl] = useState();
+
+  const getPokemons = async (res) => {
+    const pokemonData = await Promise.all(
+      res.map(async (pokemon) => {
+        const result = await axios.get(pokemon.url);
+        return result.data;
+      })
+    );
+    setPokemonList(pokemonData);
+  };
+
+  const pokemonFetch = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.get(url);
+      setNextUrl(res.data.next);
+      setPreviousUrl(res.data.previous);
+      getPokemons(res.data.results);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    pokemonFetch();
+  }, [url]);
+
+  let closeInfo = () => {
+    setPokemonInfo("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+         <div className="App">
+           {loading ? <h1>"LOADING"</h1> : <PokeDex pokemonList={pokemonList} />}
+      {pokemonInfo ? (
+        <PokeInfo
+          name={pokemonInfo.name}
+          sprite={pokemonInfo.sprites.other["official-artwork"].front_default}
+          types={pokemonInfo.types}
+          stats={pokemonInfo.stats}
+          closeInfo={closeInfo}
+        />
+      ) : (
+        ""
+      )}
+         </div>
+
+     
   );
 }
 
-export default App;
+export default App;
